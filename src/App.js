e8297063cb0c2b67fad28ed35de8329a5f782ef8
Frank <franciscotame0@gmail.com>
2023-03-04 22:51:33 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react'
 import './App.css';
+import Square from './Components/Square';
+
 
 function App() {
+
+  const [board, setBoard] = useState(["X", "", "","", "O", "","", "", "X"])
+
+  const chooseSquare = (square) => { //square is the index of each square
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='board'>
+        <div className='row'>
+          <Square val = {board[0]} chooseSquare = {() => {chooseSquare(0);}}/>
+          <Square val = {board[1]} chooseSquare = {() => {chooseSquare(0);}}/>
+          <Square val = {board[2]} chooseSquare = {() => {chooseSquare(0);}}/>
+        </div>
+        <div className='row'>
+          <Square val = {board[3]} chooseSquare = {() => {alert(3)}}/>
+          <Square val = {board[4]} chooseSquare = {() => {alert(4)}}/>
+          <Square val = {board[5]} chooseSquare = {() => {alert(5)}}/>
+        </div>
+        <div className='row'>
+          <Square val = {board[6]} chooseSquare = {() => {alert(6)}}/>
+          <Square val = {board[7]} chooseSquare = {() => {alert(7)}}/>
+          <Square val = {board[8]} chooseSquare = {() => {alert(8)}}/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Square.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App.css'
+
+const Square = ({val, chooseSquare}) => { //We need the value and a function for when a square is chosen
+  return (
+    <div className='square' onClick={chooseSquare}>{val}</div> //When we click on it, we call the function and the value is displayed
+  )
+}
+
+export default Square
