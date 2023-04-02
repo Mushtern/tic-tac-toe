02f79966844e723551690c662e4a78b5866ce112
Frank <franciscotame0@gmail.com>
2023-04-02 13:52:29 -0600

    Base game finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,80 @@
 import React from 'react';
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './App.css';
 import Square from './Components/Square';
+import {WinningPatterns} from './WinningPatterns'
 
 
 function App() {
 
-  const [board, setBoard] = useState(["X", "", "","", "O", "","", "", "X"])
+  const [board, setBoard] = useState(["", "", "","", "", "","", "", ""]);
+  const [player, setPlayer] = useState("X");
+  const [result, setResult] = useState({winner: '', state: ''});
 
-  const chooseSquare = (square) => { //square is the index of each square
+  useEffect (() => {
+    checkWin();
+    checkTie();
+  }, [board]);
 
+  useEffect (() => {
+    if(result.state !== '' && result.state !== 'Tie') alert(`Game over! ${result.winner} won.`);
+    else if (result.state !== '' && result.state !== 'Win') alert(`Game over! It's a tie!`);
+    restart();
+  }, [result]);
+
+  const chooseSquare = (index) => { //square is the index of each square
+    setBoard(board.map((val, idx) => {
+      if (idx  === index && val === ""){ //This lets us only return the player on empty squares.
+        return player; //We set the element selected to the player.
+      }
+      return val;
+    }));
+
+    board.map((val, idx) => {
+      if(idx === index && val === "" && player === "X"){
+        setPlayer("O");
+      }
+
+      else if (idx === index && val === "" && player === "O"){
+        setPlayer("X");
+      }
+    })
+  }
+
+  const checkWin = () => {
+    WinningPatterns.forEach((pattern => {
+      const firstPlayer = board[pattern[0]];
+      if(firstPlayer === "") return;
+      let someoneWon = true;
+      pattern.forEach((index => {
+        if(board[index] !== firstPlayer){
+          someoneWon = false;
+        }
+      }))
+
+      if(someoneWon === true){
+        //Someone won.
+        setResult({winner: firstPlayer, state: "Win"});
+      }
+    }))
+  }
+
+  const checkTie = () => {
+    let boardFilled = true;
+    board.forEach((square) => {
+      if(square === ""){
+        boardFilled = false;
+      }
+    })
+
+    if(boardFilled){
+      setResult({winner: 'No one', state: 'Tie'});
+    }
+  }
+
+  const restart = () => {
+    setBoard(["", "", "","", "", "","", "", ""]);
+    setPlayer("X");
   }
 
   return (
@@ -17,18 +82,18 @@ function App() {
       <div className='board'>
         <div className='row'>
           <Square val = {board[0]} chooseSquare = {() => {chooseSquare(0);}}/>
-          <Square val = {board[1]} chooseSquare = {() => {chooseSquare(0);}}/>
-          <Square val = {board[2]} chooseSquare = {() => {chooseSquare(0);}}/>
+          <Square val = {board[1]} chooseSquare = {() => {chooseSquare(1);}}/>
+          <Square val = {board[2]} chooseSquare = {() => {chooseSquare(2);}}/>
         </div>
         <div className='row'>
-          <Square val = {board[3]} chooseSquare = {() => {alert(3)}}/>
-          <Square val = {board[4]} chooseSquare = {() => {alert(4)}}/>
-          <Square val = {board[5]} chooseSquare = {() => {alert(5)}}/>
+          <Square val = {board[3]} chooseSquare = {() => {chooseSquare(3);}}/>
+          <Square val = {board[4]} chooseSquare = {() => {chooseSquare(4);}}/>
+          <Square val = {board[5]} chooseSquare = {() => {chooseSquare(5);}}/>
         </div>
         <div className='row'>
-          <Square val = {board[6]} chooseSquare = {() => {alert(6)}}/>
-          <Square val = {board[7]} chooseSquare = {() => {alert(7)}}/>
-          <Square val = {board[8]} chooseSquare = {() => {alert(8)}}/>
+          <Square val = {board[6]} chooseSquare = {() => {chooseSquare(6);}}/>
+          <Square val = {board[7]} chooseSquare = {() => {chooseSquare(7);}}/>
+          <Square val = {board[8]} chooseSquare = {() => {chooseSquare(8);}}/>
         </div>
       </div>
     </div>
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -3,7 +3,9 @@ import '../App.css'
 
 const Square = ({val, chooseSquare}) => { //We need the value and a function for when a square is chosen
   return (
-    <div className='square' onClick={chooseSquare}>{val}</div> //When we click on it, we call the function and the value is displayed
+    <div className='square' onClick={chooseSquare}>
+      {val}
+    </div> //When we click on it, we call the function and the value is displayed
   )
 }
 
